0c6847d1378d3bf2394b76a88f5ed2bdb42b85a0
805 Shutters <805shutters@gmail.com>
2026-06-27 20:06:44 -0700

    Add 41 plus booking count option

--- src/components/booking/BookingCalendar.tsx
+++ src/components/booking/BookingCalendar.tsx
@@ -45,7 +45,8 @@ const windowCountOptions = [
   { label: "21-25", value: "25" },
   { label: "26-30", value: "30" },
   { label: "31-35", value: "35" },
-  { label: "36-40", value: "40" }
+  { label: "36-40", value: "40" },
+  { label: "41+", value: "41" }
 ];
 
 function currentMonth() {
